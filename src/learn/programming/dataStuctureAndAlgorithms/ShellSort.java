cca0d62529ffc2897f819062ab0c94b9a86046c7
Tat Tran <tat.tran@wisecommerce.io>
2021-08-30 18:35:28 +0700

    add Shell Sort

---
+++ src/learn/programming/dataStuctureAndAlgorithms/ShellSort.java
@@ -0,0 +1,24 @@
+package learn.programming.dataStuctureAndAlgorithms;
+
+import java.util.Arrays;
+
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] intArray = {23, 1, 55, -5, 0, 77, -99};
+        System.out.println("Original Array: " + Arrays.toString(intArray));
+
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < intArray.length; i++) {
+                int newElement = intArray[i];
+                int j = i;
+                while (j >= gap && intArray[j - gap] > newElement) {
+                    intArray[j] = intArray[j - gap];
+                    j -= gap;
+                }
+                intArray[j] = newElement;
+            }
+        }
+
+        System.out.println("Sorted Array: " + Arrays.toString(intArray));
+    }
+}
