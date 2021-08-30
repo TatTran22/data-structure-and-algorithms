7a50fc2faa06ad867969dbd845d48ddc27af10d2
Tat Tran <tat.tran@wisecommerce.io>
2021-08-30 18:07:21 +0700

    init project with Bubble Sort

---
+++ src/learn/programming/dataStuctureAndAlgorithms/BubbleSort.java
@@ -0,0 +1,31 @@
+package learn.programming.dataStuctureAndAlgorithms;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] intArray = {23, 1, 55, -5, 0, 77, -99};
+        System.out.println("Original Array: " + Arrays.toString(intArray));
+
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[i + 1]) {
+                    swap(intArray, i, i + 1);
+                }
+            }
+        }
+
+        System.out.println("Sorted Array: " + Arrays.toString(intArray));
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
