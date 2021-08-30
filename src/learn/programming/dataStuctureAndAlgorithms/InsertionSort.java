3dd67184b92e344dc5cdb07aee241503166b16e4
Tat Tran <tat.tran@wisecommerce.io>
2021-08-30 18:24:14 +0700

    add Insertion Sort

---
+++ src/learn/programming/dataStuctureAndAlgorithms/InsertionSort.java
@@ -0,0 +1,23 @@
+package learn.programming.dataStuctureAndAlgorithms;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args){
+        int[] intArray = {23, 1, 55, -5, 0, 77, -99};
+        System.out.println("Original Array: " + Arrays.toString(intArray));
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+            int newElement = intArray[firstUnsortedIndex];
+
+            int i;
+            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+                intArray[i] = intArray[i - 1];
+            }
+
+            intArray[i] = newElement;
+        }
+
+        System.out.println("Sorted Array: " + Arrays.toString(intArray));
+    }
+}
