0d71b6b4acae5fe7e4fcc6abfa6f34b853d2bb76
Tat Tran <tat.tran@wisecommerce.io>
2021-08-30 18:08:26 +0700

    add Selection Sort

---
+++ src/learn/programming/dataStuctureAndAlgorithms/SelectionSort.java
@@ -0,0 +1,32 @@
+package learn.programming.dataStuctureAndAlgorithms;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args){
+        int[] intArray = {23, 1, 55, -5, 0, 77, -99};
+        System.out.println("Original Array: " + Arrays.toString(intArray));
+
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int maxIndex = 0;
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[maxIndex]) {
+                    maxIndex = i;
+                }
+            }
+
+            swap(intArray, maxIndex, lastUnsortedIndex);
+        }
+        System.out.println("Sorted Array: " + Arrays.toString(intArray));
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
