384e0d0270cf53dd758bf72e775b01ed8c330dcf
Tat Tran <tat.tran@wisecommerce.io>
2021-09-03 23:29:02 +0700

    add Quick Sort

---
+++ src/learn/programming/sortAlgorithms/Quick.java
@@ -0,0 +1,44 @@
+package learn.programming.sortAlgorithms;
+
+import java.util.Arrays;
+
+public class Quick {
+    public static void main(String[] args) {
+        int[] intArray = {23, 1, 55, -5, 0, 77, -99};
+        System.out.println("Original Array: " + Arrays.toString(intArray));
+        QuickSort(intArray, 0, intArray.length);
+        System.out.println("Sorted Array: " + Arrays.toString(intArray));
+    }
+
+    public static void QuickSort(int[] input, int start, int end){
+        if (end - start < 2) {
+            return;
+        }
+        int pivotIndex = partition(input, start, end);
+        QuickSort(input, start, pivotIndex);
+        QuickSort(input, pivotIndex + 1, end);
+    }
+
+    public static int partition(int[] input, int start, int end) {
+        int pivot = input[start];
+        int i = start;
+        int j = end;
+
+        while (i < j) {
+            // NOTE: empty loop body
+            while (i < j && input[--j] >= pivot) ;
+            if (i < j) {
+                input[i] = input[j];
+            }
+
+            // NOTE: empty loop body
+            while (i < j && input[++i] <= pivot) ;
+            if (i < j) {
+                input[j] = input[i];
+            }
+
+        }
+        input[j] = pivot;
+        return j;
+    }
+}
